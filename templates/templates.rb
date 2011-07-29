0adf31752aaab4c2b0e1497c84fc04dcb851e524
mxia <mxia@aptana.com>
2011-07-29 15:22:13 -0700

    Commented out the template definition until it has some content (related to APSTUD-3106).

--- templates/templates.rb
+++ templates/templates.rb
@@ -1,6 +1,7 @@
 require 'ruble'
 
-template "SASS Template" do |t|
-  t.filetype = "*.sass"
-  t.location = "templates/template.sass"
-end
+# Commenting out template code until we have actual default content to use
+#template "SASS Template" do |t|
+#  t.filetype = "*.sass"
+#  t.location = "templates/template.sass"
+#end
